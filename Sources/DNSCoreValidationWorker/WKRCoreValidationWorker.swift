2b2af28231dbbe2f71528c89a3bee8d8e4b84c00
Darren Ehlers <me@darrenehlers.com>
2021-03-25 14:40:38 -0500

    Updated to new Validation Protocol

--- Sources/DNSCoreValidationWorker/WKRCoreValidationWorker.swift
+++ Sources/DNSCoreValidationWorker/WKRCoreValidationWorker.swift
@@ -16,146 +16,248 @@ import Foundation
 
 open class WKRCoreValidationWorker: WKRBlankValidationWorker
 {
-    public enum Regex {
-        static let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        static let phone = "^[0-9]{10}$"
-    }
-
-    public var emailRegex: String = Regex.email
-    public var phoneRegex: String = Regex.phone
-
     public var passwordStrengthWorker: PTCLPasswordStrength_Protocol = WKRCrashPasswordStrengthWorker()
 
-    public required init() {
-        super.init()
-        self.commonInit()
-    }
-    public required init(nextWorker: PTCLValidation_Protocol) {
-        super.init(nextWorker: nextWorker)
-        self.commonInit()
-    }
-    func commonInit() {
-        self.minimumBirthdateAge = -1
-        self.maximumBirthdateAge = -1
-        
-        self.minimumHandleLength = 6
-        self.maximumHandleLength = 80
-        
-        self.minimumNameLength = 2
-        self.maximumNameLength = 250
-        
-        self.minimumNumberValue = -1
-        self.maximumNumberValue = -1
-        
-        self.minimumPercentageValue = -1
-        self.maximumPercentageValue = -1
-        
-        self.minimumPhoneLength = 10
-        self.maximumPhoneLength = 10
-        
-        self.minimumUnsignedNumberValue = -1
-        self.maximumUnsignedNumberValue = -1
-    }
-
     // MARK: - Business Logic / Single Item CRUD
-    override open func doValidateBirthdate(for birthdate: Date) throws -> DNSError? {
-        return nil
-    }
-    override open func doValidateEmail(for email: String) throws -> DNSError? {
-        guard !email.isEmpty else {
+    override open func doValidateBirthdate(for birthdate: Date?,
+                                           with config: PTCLValidationBirthdateConfig) throws -> DNSError? {
+        guard let birthdate = birthdate else {
             return PTCLValidationError
                 .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard email.dnsCheck(regEx: self.emailRegex) else {
+//        guard config.minimumAge == nil || number >= config.minimumAge! else {
+//            return PTCLValidationError
+//                .tooLow(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+//        guard config.maximumAge == nil || number <= config.maximumAge! else {
+//            return PTCLValidationError
+//                .tooHigh(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+        return nil
+    }
+    override open func doValidateDate(for date: Date?,
+                                      with config: PTCLValidationDateConfig) throws -> DNSError? {
+        guard let date = date else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+//        guard config.minimum == nil || number >= config.minimum! else {
+//            return PTCLValidationError
+//                .tooLow(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+//        guard config.maximum == nil || number <= config.maximum! else {
+//            return PTCLValidationError
+//                .tooHigh(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+        return nil
+    }
+    override open func doValidateEmail(for email: String?,
+                                       with config: PTCLValidationEmailConfig) throws -> DNSError? {
+        guard let email = email else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard !config.required || !email.isEmpty else {
+            return PTCLValidationError
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.regex == nil || email.dnsCheck(regEx: config.regex!) else {
             return PTCLValidationError
                 .invalid(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
         return nil
     }
-    override open func doValidateHandle(for handle: String) throws -> DNSError? {
-        guard self.minimumHandleLength == -1 ||
-                handle.count >= self.minimumHandleLength else {
+    override open func doValidateHandle(for handle: String?,
+                                        with config: PTCLValidationHandleConfig) throws -> DNSError? {
+        guard let handle = handle else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard !config.required || !handle.isEmpty else {
+            return PTCLValidationError
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.minimumLength == nil || handle.count >= config.minimumLength! else {
             return PTCLValidationError
                 .tooShort(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard self.maximumHandleLength == -1 ||
-                handle.count <= self.maximumHandleLength else {
+        guard config.maximumLength == nil || handle.count <= config.maximumLength! else {
             return PTCLValidationError
                 .tooLong(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
+        guard config.regex == nil || handle.dnsCheck(regEx: config.regex!) else {
+            return PTCLValidationError
+                .invalid(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
         return nil
     }
-    override open func doValidateName(for name: String) throws -> DNSError? {
-        guard self.minimumHandleLength == -1 ||
-                name.count >= self.minimumNameLength else {
+    override open func doValidateName(for name: String?,
+                                      with config: PTCLValidationNameConfig) throws -> DNSError? {
+        guard let name = name else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard !config.required || !name.isEmpty else {
+            return PTCLValidationError
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.minimumLength == nil || name.count >= config.minimumLength! else {
             return PTCLValidationError
                 .tooShort(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard self.maximumHandleLength == -1 ||
-                name.count <= self.maximumNameLength else {
+        guard config.maximumLength == nil || name.count <= config.maximumLength! else {
             return PTCLValidationError
                 .tooLong(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
+        guard config.regex == nil || name.dnsCheck(regEx: config.regex!) else {
+            return PTCLValidationError
+                .invalid(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
         return nil
     }
-    override open func doValidateNumber(for number: String) throws -> DNSError? {
+    override open func doValidateNumber(for number: String?,
+                                        with config: PTCLValidationNumberConfig) throws -> DNSError? {
+        guard let number = number else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+//        guard config.minimum == nil || number >= config.minimum! else {
+//            return PTCLValidationError
+//                .tooLow(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+//        guard config.maximum == nil || number <= config.maximum! else {
+//            return PTCLValidationError
+//                .tooHigh(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
         return nil
     }
-    override open func doValidatePassword(for password: String) throws -> DNSError? {
+    override open func doValidatePassword(for password: String?,
+                                          with config: PTCLValidationPasswordConfig) throws -> DNSError? {
+        guard let password = password else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard !config.required || !password.isEmpty else {
+            return PTCLValidationError
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.minimumLength == nil || password.count >= config.minimumLength! else {
+            return PTCLValidationError
+                .tooShort(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.maximumLength == nil || password.count <= config.maximumLength! else {
+            return PTCLValidationError
+                .tooLong(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
         let strength = try! self.passwordStrengthWorker.doCheckPasswordStrength(for: password)
-        guard strength.rawValue >= requiredPasswordStrength.rawValue else {
+        guard strength.rawValue >= config.strength.rawValue else {
             return PTCLValidationError
                 .tooWeak(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
         return nil
     }
-    override open func doValidatePercentage(for percentage: String) throws -> DNSError? {
-        return nil
-    }
-    override open func doValidatePhone(for phone: String) throws -> DNSError? {
-        guard !phone.isEmpty else {
+    override open func doValidatePercentage(for percentage: String?,
+                                            with config: PTCLValidationPercentageConfig) throws -> DNSError? {
+        guard let percentage = percentage else {
             return PTCLValidationError
                 .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard self.minimumPhoneLength == -1 ||
-                phone.count >= self.minimumPhoneLength else {
+//        guard config.minimum == nil || number >= config.minimum! else {
+//            return PTCLValidationError
+//                .tooLow(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+//        guard config.maximum == nil || number <= config.maximum! else {
+//            return PTCLValidationError
+//                .tooHigh(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+        return nil
+    }
+    override open func doValidatePhone(for phone: String?,
+                                       with config: PTCLValidationPhoneConfig) throws -> DNSError? {
+        guard let phone = phone else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard !config.required || !phone.isEmpty else {
+            return PTCLValidationError
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.minimumLength == nil || phone.count >= config.minimumLength! else {
             return PTCLValidationError
                 .tooShort(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard self.maximumPhoneLength == -1 ||
-                phone.count <= self.maximumPhoneLength else {
+        guard config.maximumLength == nil || phone.count <= config.maximumLength! else {
             return PTCLValidationError
                 .tooLong(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard phone.dnsCheck(regEx: self.phoneRegex) else {
+        guard config.regex == nil || phone.dnsCheck(regEx: config.regex!) else {
             return PTCLValidationError
                 .invalid(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
         return nil
     }
-    override open func doValidateSearch(for search: String) throws -> DNSError? {
-        guard !search.isEmpty else {
+    override open func doValidateSearch(for search: String?,
+                                        with config: PTCLValidationSearchConfig) throws -> DNSError? {
+        guard let search = search else {
             return PTCLValidationError
                 .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        return nil
-    }
-    override open func doValidateState(for state: String) throws -> DNSError? {
-        guard !state.isEmpty else {
+        guard !config.required || !search.isEmpty else {
             return PTCLValidationError
-                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard state.count >= 2 else {
+        guard config.minimumLength == nil || search.count >= config.minimumLength! else {
             return PTCLValidationError
                 .tooShort(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
-        guard state.count <= 2 else {
+        guard config.maximumLength == nil || search.count <= config.maximumLength! else {
             return PTCLValidationError
                 .tooLong(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
         }
+        guard config.regex == nil || search.dnsCheck(regEx: config.regex!) else {
+            return PTCLValidationError
+                .invalid(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
         return nil
     }
-    override open func doValidateUnsignedNumber(for number: String) throws -> DNSError? {
+    override open func doValidateState(for state: String?,
+                                       with config: PTCLValidationStateConfig) throws -> DNSError? {
+        guard let state = state else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard !config.required || !state.isEmpty else {
+            return PTCLValidationError
+                .required(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.minimumLength == nil || state.count >= config.minimumLength! else {
+            return PTCLValidationError
+                .tooShort(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.maximumLength == nil || state.count <= config.maximumLength! else {
+            return PTCLValidationError
+                .tooLong(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        guard config.regex == nil || state.dnsCheck(regEx: config.regex!) else {
+            return PTCLValidationError
+                .invalid(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+        return nil
+    }
+    override open func doValidateUnsignedNumber(for number: String?,
+                                                with config: PTCLValidationUnsignedNumberConfig) throws -> DNSError? {
+        guard let number = number else {
+            return PTCLValidationError
+                .noValue(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+        }
+//        guard config.minimum == nil || number >= config.minimum! else {
+//            return PTCLValidationError
+//                .tooLow(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
+//        guard config.maximum == nil || number <= config.maximum! else {
+//            return PTCLValidationError
+//                .tooHigh(DNSCoreValidationWorkerCodeLocation(self, "\(#file),\(#line),\(#function)"))
+//        }
         return nil
     }
 }
